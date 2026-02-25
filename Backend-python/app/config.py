1e0863cd8adc09ef9265c1185ae6c8f9f45f0164
Yair Lopez <yahirlopez380@gmail.com>
2026-02-25 16:59:06 -0600

    feat: ordenando archivos

--- Backend-python/app/config.py
+++ Backend-python/app/config.py
@@ -5,11 +5,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_connection():
-    return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        port=os.getenv("DB_PORT"),
-        sslmode="require"
-    )
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            port=os.getenv("DB_PORT"),
+            sslmode="require"
+        )
+        return conn
+    except Exception as e:
+        print("Error conectando a la base de datos:", e)
+        raise
